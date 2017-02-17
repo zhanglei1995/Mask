0aa1d3d634001a80c79e3fc3c503312e41edbe8f
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2017-02-17 23:27:02 +0800

    auto rebuild

--- src/hooks/post-receive.js
+++ src/hooks/post-receive.js
@@ -1,17 +1,39 @@
 import Git from 'nodegit'
+import path from 'path'
+import { buildAll } from '../build'
+import { extractSlug } from '../utils/markdown-parser'
+
+function getRootDir(pathname) {
+  while (true) {
+    let dir = path.dirname(pathname)
+    if (dir === '.') {
+      return pathname
+    } else {
+      pathname = dir
+    }
+  }
+}
 
 async function postReceiveHandler() {
   let repoData = await Git.Repository.open('../data')
   await repoData.fetchAll()
   await repoData.mergeBranches('master', 'origin/master')
   let commit = await repoData.getHeadCommit()
+    , needRemoveSlugs = []
   for (let diff of await commit.getDiff()) {
     for (let patch of await diff.patches()) {
-      console.log('isAdded: ', patch.isAdded())
-      console.log('isDeleted: ', patch.isDeleted())
-      console.log('oldFile path: ', patch.oldFile().path())
-      console.log('newFile path: ', patch.newFile().path())
+      let newFilePath = patch.newFile().path()
+        , oldFilePath = patch.oldFile().path()
+        , newFileRootDir = getRootDir(newFilePath)
+        , oldFileRootDir = getRootDir(oldFilePath)
+      if (oldFileRootDir === 'content' && (patch.isDeleted() || patch.isRenamed())) {
+        needRemove.push(extractSlug(oldFilePath))
+      }
     }
+  }
+  await buildAll()
+  for (let slug of needRemoveSlugs) {
+    await fs.removeAsync(`../public/${ slug }.html`)
   }
 }
 
